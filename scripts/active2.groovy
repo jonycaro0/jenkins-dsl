289251abab82631dd5752a4d872bf7a6231788c6
jonycaro <jonycaro@ElZarabambiche.localdomain>
2022-06-15 01:14:37 -0500

    2

--- scripts/active2.groovy
+++ scripts/active2.groovy
@@ -1,29 +1,98 @@
-folder('choice') {
-    displayName('choice')
-    description('choice')
+import java.util.Arrays;
+
+import java.util.ArrayList;
+
+import groovy.transform.Field
+
+
+
+@Field def list1=[]
+
+@Field def list2=[]
+
+folder('PRUEBAS'){
+
+    displayName('PRUEBAS')
+
+    description('Jobs de Pruebas')
+
 }
 
-pipelineJob('choice/choice2') {
-    displayName('choice2')
 
-    definition {
-        cpsScm {
-            scm {
-                git {
-                    branch('feature/seedJob')
-                    remote {
-                        url('https://github.com/jonycaro0/jenkins-dsl.git')
-                    }
-                }
+
+
+
+pipelineJob('PRUEBAS/prueba2'){
+
+    displayName('prueba2')
+
+
+
+
+    logRotator{
+
+        numToKeep(5)
+
+    }
+
+    parameters {
+
+        addList()
+
+        choiceParam('TIPO', ['PIPELINE', 'DATASET'], 'seleccion')
+
+       
+
+        activeChoiceReactiveParam('CHOICE-1') {
+
+            description('Allows user choose from multiple choices')
+
+            filterable()
+
+            choiceType('SINGLE_SELECT')
+
+            groovyScript {
+
+
+
+                script """if(TIPO == "PIPELINE") {return ${list1}}else if(TIPO == "DATASET") {return ${list2}} """
+
+                               
+
+                fallbackScript('"fallback choice"')
+
             }
 
-            scriptPath('jobs/choice/choice2.groovy')
+            referencedParameter('TIPO')
+
         }
+
+       
+
+       
+
+         
+
+       
+
     }
 
-    logRotator {
-        numToKeep(5)
-    }
+   
 
+}
+
+def addList(){
+
+    list1.add('1')
+
+    list1.add('2')
+
+    list1.add('3')
+
+    list2.add('\'a\'')
+
+    list2.add('\'b\'')
+
+    list2.add('\'c\'')
 
 }
